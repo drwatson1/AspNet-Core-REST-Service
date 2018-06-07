6e4461e56ffb77d9056547f0ba6b577bf9a37b7c
Sergey Tregub <drwatson1@yandex.ru>
2018-06-07 15:06:20 +0300

    feat: Support Serilog logging

--- WebApplication15/Startup.cs
+++ WebApplication15/Startup.cs
@@ -1,9 +1,13 @@
-﻿using System.Reflection;
+﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Web.Http;
 using Autofac;
 using Autofac.Integration.WebApi;
 using Microsoft.Owin;
 using Owin;
+using Serilog;
+using static System.Environment;
 
 [assembly: OwinStartup(typeof(WebApplication15.Startup))]
 
@@ -16,13 +20,33 @@ namespace WebApplication15
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
             HttpConfiguration config = new HttpConfiguration();
             ConfigureAutofac(app, config);
+            ConfigureLogger(app, config);
 
             WebApiConfig.Register(config);
 
             app.UseWebApi(config);
         }
 
-        public void ConfigureAutofac(IAppBuilder app, HttpConfiguration config)
+        private void ConfigureLogger(IAppBuilder app, HttpConfiguration config)
+        {
+            // Use Seriog for logging
+            // More information can be found here https://github.com/serilog/serilog/wiki/Getting-Started
+
+            var f = new FileInfo(Assembly.GetExecutingAssembly().Location);
+            var name = f.Name.Replace(f.Extension, "");
+
+            // TODO: Adjust log file location and name. 
+            // By default log file is located in 'C:\Users\<username>\AppData\Roaming\Logs' folder and named as the current assembly name
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(ExpandEnvironmentVariables($"%AppData%/Logs/{name}..txt"), rollingInterval: RollingInterval.Day)
+                .ReadFrom.AppSettings()
+                .CreateLogger();
+
+            Log.Information("aaa");
+            Log.Debug("bbb");
+        }
+
+        private void ConfigureAutofac(IAppBuilder app, HttpConfiguration config)
         {
             var builder = new ContainerBuilder();
 
@@ -38,7 +62,7 @@ namespace WebApplication15
             app.UseAutofacWebApi(config);
         }
 
-        public void RegisterDiServices(ContainerBuilder builder)
+        private void RegisterDiServices(ContainerBuilder builder)
         {
             // TODO: Place your code here
         }
