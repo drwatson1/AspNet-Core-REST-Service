952b915b6950fff214a54393fa710bcbd45ba6ef
Sergey Tregub <drwatson1@yandex.ru>
2018-06-07 12:37:41 +0300

    Переходим на Owin

--- WebApplication15/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication15.WebApiApplication" Language="C#" %>
---
+++ WebApplication15/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication15.Startup))]
+
+namespace WebApplication15
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            HttpConfiguration config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+    }
+}
