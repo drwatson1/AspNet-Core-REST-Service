e03ec4b16cf3b0d5e1c4f6072a740e83058f58f3
Sergey Tregub <drwatson1@yandex.ru>
2018-06-07 12:53:56 +0300

    Поддержка Autofac

--- WebApplication15/Startup.cs
+++ WebApplication15/Startup.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
 using Microsoft.Owin;
 using Owin;
 
@@ -12,10 +15,32 @@ namespace WebApplication15
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
             HttpConfiguration config = new HttpConfiguration();
+            ConfigureAutofac(app, config);
 
             WebApiConfig.Register(config);
 
             app.UseWebApi(config);
+        }
+
+        public void ConfigureAutofac(IAppBuilder app, HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()).InstancePerRequest();
+            RegisterDiServices(builder);
+
+            var container = builder.Build();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+        }
+
+        public void RegisterDiServices(ContainerBuilder builder)
+        {
+            // TODO: Place your code here
         }
     }
 }
