d13b00a53fdf4ca4cbd922d92db595a06ce6ee79
Sergey Tregub <drwatson1@yandex.ru>
2018-06-25 16:31:35 +0300

    improvement: Use XML-comments with Swagger

--- ProjectTemplates/AspNet.WebApi/ReferenceProject/ReferenceProject/App_Start/SwaggerConfig.cs
+++ ProjectTemplates/AspNet.WebApi/ReferenceProject/ReferenceProject/App_Start/SwaggerConfig.cs
@@ -1,5 +1,7 @@
 ﻿using Swashbuckle.Application;
 using System;
+using System.IO;
+using System.Reflection;
 using System.Web.Http;
 
 namespace ReferenceProject
@@ -11,8 +13,25 @@ namespace ReferenceProject
             if (config == null)
                 throw new ArgumentNullException(nameof(config));
 
+            // Use http://localhost:5000/swagger/ui/index to inspect API docs
             config
-                .EnableSwagger(c => { c.PrettyPrint(); c.SingleApiVersion("v1", "A title for your API"); })
+                .EnableSwagger(c =>
+                {
+                    c.SingleApiVersion("v1", "A title for your API");
+                    c.PrettyPrint();
+                    c.RootUrl(x =>
+                    {
+                        var idx = x.RequestUri.AbsoluteUri.IndexOf("swagger", StringComparison.InvariantCultureIgnoreCase);
+                        return x.RequestUri.AbsoluteUri.Substring(0, idx - 1);
+                    });
+
+                    // This code allow you to use XML-comments
+                    var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                    var commentsFileName = Assembly.GetExecutingAssembly().GetName().Name + ".XML";
+                    var commentsFile = Path.Combine(baseDirectory, "bin/" + commentsFileName);
+
+                    c.IncludeXmlComments(commentsFile);
+                })
                 .EnableSwaggerUi();
         }
     }
