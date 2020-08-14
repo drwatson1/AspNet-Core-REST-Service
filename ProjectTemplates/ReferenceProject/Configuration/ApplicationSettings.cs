13d4ae0840780ad001b1378a5fbc540bc9fde2da
Sergey Tregub <sergey.tregub@vocord.ru>
2020-08-14 18:35:40 +0300

    Add comments

--- ProjectTemplates/ReferenceProject/Configuration/ApplicationSettings.cs
+++ ProjectTemplates/ReferenceProject/Configuration/ApplicationSettings.cs
@@ -7,6 +7,7 @@ namespace ReferenceProject.Configuration
     {
         public static void AddSettings(this IServiceCollection services)
         {
+            // Uses AutoBind and SubstituteVariables from https://github.com/drwatson1/configuration-extensions project
             services.AddOptions<Settings.Products>()
                 .AutoBind()
                 .SubstituteVariables();
